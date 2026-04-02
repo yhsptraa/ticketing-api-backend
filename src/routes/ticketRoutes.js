f1a586e08b59016c451b23751034cb97a470b65f
morrowpappitson <60286396+tiaraalviana878@users.noreply.github.com>
2026-04-02 19:37:33 +0700

    add ticket detail endpoint

--- src/routes/ticketRoutes.js
+++ src/routes/ticketRoutes.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Ticket = require('../models/ticket');
 
 router.get('/tickets', (req, res) => {
-    res.json([
-        {
-            event: 'Concert',
-            price: 50000
-        }
-    ]);
+    res.json([{ event: "Concert", price: 50000 }]);
 });
 
-router.post('/tickets', (req, res) => {
+router.get('/tickets/:id', (req, res) => {
     res.json({
-        message: 'Ticket created',
-        data: req.body
+        id: req.params.id,
+        event: "Concert",
+        price: 50000
     });
+});
+
+router.post('/tickets', async (req, res) => {
+    const ticket = new Ticket(req.body);
+    await ticket.save();
+    res.json(ticket);
 });
 
 module.exports = router;
